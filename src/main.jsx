28c54a20d6a4ce219e3369c78e7366bf54a8fa0f
Yusuf Sidik <yusufsidik36@gmail.com>
2023-11-04 20:33:22 +0800

    create header section

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import Navbar from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <div className='w-full h-screen bg-slate-200 font-poppins'>
     <div className='relative bg-[#FFF2E1]'>
       <Navbar></Navbar>
+      <Header></Header>
     </div>
   </div>
   // <React.StrictMode>
